38885823d6775a00f8939384258b0a068af24d33
Naadiyaar <naadiyaar1@gmail.com>
2022-11-22 23:36:30 +0330

    q4 answer

---
+++ chapter_1/q4.java
@@ -0,0 +1,19 @@
+// Qustion: write following experetions in java form
+
+/*
+ * a => (b^2-4ac)/2a
+ * b => -b+(b^2-4ac)/2a
+ */
+
+
+
+
+
+
+
+
+
+/*
+ * a => (b ** 2 - 4 * a * c) / 2 * a;
+ * b => -b + (b ** 2 - 4 * a * c) / 2 * a;
+ */
