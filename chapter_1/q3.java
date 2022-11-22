2066f17fe804fb9429eeeb1a238773d366acd4e8
Naadiyaar <naadiyaar1@gmail.com>
2022-11-22 23:28:58 +0330

    q3 answer

---
+++ chapter_1/q3.java
@@ -0,0 +1,24 @@
+// Qusitiion: which of the following statments are false? say why
+
+/*
+ * a => n = 12
+ * b => char letter = ;
+ * c => int one = 50 two;
+ * d => double x, y, z;
+ */
+
+
+
+
+
+
+
+
+
+ /* Answer:
+ * a => unspecified varible type, and the symbole ';' in the end is needed.
+ * 
+ * b => one more ';' in the end is needed.
+ * 
+ * c => symbole comma',' betwwen variable names is needed.
+ */
