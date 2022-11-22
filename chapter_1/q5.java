9b57bd4c1b3e45993d2dcf0ce4d40d878f433914
Naadiyaar <naadiyaar1@gmail.com>
2022-11-22 23:40:36 +0330

    q5 answer

---
+++ chapter_1/q5.java
@@ -0,0 +1,14 @@
+// Qusition: why do we say java is platform-independent?
+
+
+
+
+/*
+ * Answer: It meant you could create a Java program on, let’s say, 
+ * Windows, compile it to bytecode, 
+ * and run the application on any other platform that 
+ * supports a Java Virtual Machine (JVM). 
+ * In this case, 
+ * a JVM serves as an abstraction level 
+ * between the code and the hardware.
+ */
