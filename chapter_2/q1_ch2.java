14b8f5b430f582d29ff8cadc464c5ee184678cce
Naadiyaar <naadiyaar1@gmail.com>
2022-11-23 11:51:04 +0330

    q1_ch2 answer

---
+++ chapter_2/q1_ch2.java
@@ -0,0 +1,30 @@
+// write a program that generate below output:
+//   *
+// *   *
+//*  *  *
+
+
+
+
+
+
+
+
+
+// Answer:
+
+public class q1_ch2 {
+    public static void main(String[] args) {
+        int i ;
+
+        System.out.println("  * ");
+        for (i=0;i<2;i++){
+        System.out.print(" *");
+        }
+
+        System.out.println("\r");
+        for (i=0;i<3;i++){
+        System.out.print("* ");
+        } 
+    } 
+}
