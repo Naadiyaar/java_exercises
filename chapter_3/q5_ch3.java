ad5d1e5e4f87e519a119c754be66c91a94a2f177
Naadiyaar <naadiyaar1@gmail.com>
2022-11-30 17:11:32 +0330

    q5_ch3 answer

---
+++ chapter_3/q5_ch3.java
@@ -0,0 +1,31 @@
+/*
+ * Question: what is the output of bellow command? solve mentally
+ * 
+ * int x = 5;
+ * while (--x > 0)
+ *      System.out.println(x);
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Answer:
+/*
+ * 4
+ * 3
+ * 2
+ * 1
+ */
