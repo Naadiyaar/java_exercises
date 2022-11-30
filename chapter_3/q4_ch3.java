3e3a56d4a53f2be8b64bb1d3e4c0142a6ca99113
Naadiyaar <naadiyaar1@gmail.com>
2022-11-30 17:15:47 +0330

    q4_ch3 fix

--- chapter_3/q4_ch3.java
+++ chapter_3/q4_ch3.java
@@ -1,3 +1,32 @@
 /*
  * Question: rewrite bellow command using for loop
+ * 
+ * int i = 1;
+ * while (i <= 101){
+ *      if (i < 5 && i != 2){
+ *      System.out.println('x');
+ *      }
+ *      i++;
+ * }
  */
+
+
+
+
+
+
+
+
+
+
+
+
+// Answer:
+public class q4_ch3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 101; i++) {
+            if (i < 5 && i != 2)
+                System.out.println('x');
+        }
+    }
+}
